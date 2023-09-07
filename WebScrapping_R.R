88b148729ea3379cd39d5fc8d53978dcc6079b3a
iurialb <iurialbuquerque@protonmail.com>
2023-09-07 18:12:10 -0300

    Create WebScrapping_R.R

---
+++ WebScrapping_R.R
@@ -0,0 +1,22 @@
+### WEB SCRAPPING NO R
+
+#Pacotes necessários
+library("httr")
+library("rvest")
+
+#Função de leitura
+wikipedia <- read_html("https://en.wikipedia.org/wiki/Web_scraping")
+
+wikipedia %>% html_elements("title")
+
+#Busca nó de heading, mas tira parte do código HTML
+wikipedia %>% html_elements("h1") %>% html_text()
+
+#Busca nó de parágrafo, mas tira parte do código HTML
+data <- wikipedia %>% html_elements("p") %>% html_text()
+
+length(data)
+
+#Visualizar os parágrafos solicitados
+data[2]
+data[5]
